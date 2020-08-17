2a2bec51a28532ff05c9af3f4dbb10bf917bfd42
owari-taro <taro.biwajima@gmail.com>
2020-08-17 22:19:06 +0900

    simulate

--- quant_econ/introductory.jl
+++ quant_econ/introductory.jl
@@ -1,10 +1,8 @@
 using InstantiateFromURL
-github_project("QuantEcon/quantecon-notebooks-julia", version = "0.8.0")
+github_project("QuantEcon/quantecon-notebooks-julia", version="0.8.0")
 randn()
 
-
-
-#Starting with the most direct version, and pretending we are in a world where randn can only return a single value
+# Starting with the most direct version, and pretending we are in a world where randn can only return a single value
 
 # poor style
 n = 10
@@ -47,35 +45,35 @@ end
 function generatedata(n)
     ϵ = randn(n) # use built in function
     return ϵ.^2
- end
+end
 data = generatedata(5)
 
 
-#simple function
+# simple function
 f(x) = x^2
 
 
 
-function fixedpointmap(f,iv,tolerance=1e-3,maxiter=1000)
+function fixedpointmap(f, iv, tolerance=1e-3, maxiter=1000)
 
-    x_old=iv
-    normdiff=inf
-    iter=1
-    while normdiff>tolerance && iter<=maxiter
-        x_new=f(x_old)
-        normdiff=norm(x_new-x_old)
-        iter+=1
+    x_old = iv
+    normdiff = inf
+    iter = 1
+    while normdiff > tolerance && iter <= maxiter
+        x_new = f(x_old)
+        normdiff = norm(x_new - x_old)
+        iter += 1
     end
-    return (x_old,normdiff,iter)
+    return (x_old, normdiff, iter)
 
-iter=1
+    iter = 1
 
 
-for i in [1,2,3]
-    println(iter)
-    iter+=1
+    for i in [1,2,3]
+        println(iter)
+        iter += 1
+    end
 end
-
 
 
 using NLsolve
@@ -94,9 +92,9 @@ println("Fixed point = $(sol.zero), and |f(x) - x| = $(norm(f(sol.zero) - sol.ze
 
 
 function factorial12(n::Int)
-    res=1
+    res = 1
     for i in 1:n
-        res*=i
+        res *= i
     end
     return res
 end
@@ -104,24 +102,24 @@ end
 
 
 function calc_pi(n::Int)
-    count=0
-    uniform_=rand(2*n)
+    count = 0
+    uniform_ = rand(2 * n)
     for i in 1:n
-        x,y=uniform_[2*(i)-1],uniform_[2*i]
-        if sqrt((x-0.5)^2+(y-0.5)^2)<=0.5
-            count+=1
+        x, y = uniform_[2 * (i) - 1], uniform_[2 * i]
+        if sqrt((x - 0.5)^2 + (y - 0.5)^2) <= 0.5
+            count += 1
         end
     end
-    return 4*count/n
+    return 4 * count / n
 end
 
-function binominal(n::Int,p::Float64)
-    count=0
-    uniform_=rand(n)
+function binominal(n::Int, p::Float64)
+    count = 0
+    uniform_ = rand(n)
     for i in 1:n
 
-        if uniform[i]<=p
-            count+=1
+        if uniform[i] <= p
+            count += 1
         end
     end
     return count
@@ -131,19 +129,30 @@ end
 
 
 function random_device()
-    randoms=rand(10)
+    randoms = rand(10)
     println(randoms)
-    count=0
+    count = 0
     for i in 1:10
-        if randoms[i]<=0.5
-            count+=1
-            if count==3
+        if randoms[i] <= 0.5
+            count += 1
+            if count == 3
                 return 1
             end
         else
-            count=0
+            count = 0
 
         end
     end
     return 0
+end
+
+
+function simulate(alpha::Float64)::Array
+    n = 200
+    x = zeros(n + 1)
+    norm = randn(n + 1)
+    for i in 2:n
+        x[i] = alpha * x[i - 1] + norm[i]
+    end
+    return x
 end
