ab011e96aa42ff620d4a50a28ddfe7a349f52fdb
owari-taro <taro.biwajima@gmail.com>
2020-08-26 23:42:02 +0900

    bracket_sign_chjange

--- algorithm_for_optimization/bisectrion.jl
+++ algorithm_for_optimization/bisectrion.jl
@@ -14,8 +14,19 @@ function bisection(f',a,b,ϵ)
             a=x
         else
             b=x
-
         end
+    end
+    return (a,b)
+end
+
+
+function bracket_sign_change(f',a,b,;k=2)
+    if a>b;a,b=b,a;end
+    center,half_width=(b-a)/2,(a+b)/2
+    while f'(a)*f'(b)>0
+        half_width*=k
+        a=center-half_width
+        b=center+half_width
     end
     return (a,b)
 end
