936dfb44bd6560696efff2c684b313dd1d69ac45
owari-taro <taro.biwajima@gmail.com>
2020-08-26 23:30:29 +0900

    bisection.jl

---
+++ algorithm_for_optimization/bisectrion.jl
@@ -0,0 +1,21 @@
+function bisection(f',a,b,ϵ)
+    if a>b;a,b=b,a;end
+    ya,yb=f'(a),f'(b)
+    if ya==0;b =a;end
+    if yb==0;a=b;end
+
+    while b-a>ϵ
+        x=(a+b)/2
+        y=f'(x)
+        if y==0
+            a,b=x,x
+            break
+        elseif sign(y)==sign(ya)
+            a=x
+        else
+            b=x
+
+        end
+    end
+    return (a,b)
+end
