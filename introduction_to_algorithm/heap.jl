9d28159b8b9a5b156742b56c4901104e03b5e892
owari-taro <taro.biwajima@gmail.com>
2020-09-04 21:50:53 +0900

    heap sort

---
+++ introduction_to_algorithm/heap.jl
@@ -0,0 +1,33 @@
+function parent(index)
+    return trunc(Int, index/2)
+end
+
+
+function left(index)
+    return 2index
+end
+
+function right(index)
+    return 2index+1
+end
+
+mutable struct Heap
+    heap_size::Integer
+    data::Array
+end
+
+function max_heaptify(heap::Heap,index::Integer)
+    l=left(index)
+    r=right(index)
+    if l<=heap.heap_size && heap.data[l]>heap.data[index]
+        largegst=l
+    else
+        largest=index
+    end
+    if r<=heap.heap_size && heap.data[r]>heap.data[largest]
+        largest=r
+    end
+    if largest!=index
+        max_heaptify(heap,largest)
+    end
+end
