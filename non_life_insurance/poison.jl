787f0ff0e92ba79f3c13627415c659a1dcf49f2f
owari-taro <taro.biwajima@gmail.com>
2020-09-15 13:20:41 +0900

    poisson

--- non_life_insurance/poison.jl
+++ non_life_insurance/poison.jl
@@ -3,9 +3,16 @@ using Distributions
 
 #probability function
 poisson(λ,k)=exp(-λ)*(λ^k)/factorial(k)
-sum=0
-for i in 1:100
-    tmp=poisson(1,i-1)
-    sum+=tmp
-    println(sum)
+λ=1
+Δt=0.001
+Δp=poisson(λ*Δt,1)
+array=[0]
+count=0
+for  i in 1:100_000_000
+    if rand()<Δp
+        #print(i,":",count)
+        count+=1
+        push!(array,count)
+    end
 end
+print(array)
